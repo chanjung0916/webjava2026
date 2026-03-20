e1adc8aa9bc1c1a5de60452cff0cf74ab81b6195
unknown <rud099087@g.shingu.ac.kr>
2026-03-20 10:00:00 +0900

    소수판별

---
+++ week03/isPrime.js
@@ -0,0 +1,17 @@
+// week03 문제 1: 소수 판별
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPrime(17)); // true
+console.log(isPrime(21)); // false
